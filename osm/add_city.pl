f8f966918be173471bb6c3bc6f145c18878139ec
mgoddard <mgoddard@cockroachlabs.com>
2024-01-27 05:52:58 -0500

    Fixing this so it will use DB_URL within the environment

--- osm/add_city.pl
+++ osm/add_city.pl
@@ -2,8 +2,24 @@
 
 # 262714<2018-12-07T09:55:55Z<917436<51.0359<-0.805651<Rising Sun<addr:city=Liphook|addr:postcode=GU30 7NA|addr:street=Milland Road|addr:suburb=Milland|amenity=pub|fhrs:id=147913|food=yes|real_ale=yes|gcp|gcp6|gcp6m|gcp6md<gcp6mdv3dvr1
 
+if (not defined $ENV{DB_URL}) {
+  die 'Environment variable DB_URL must be defined; e.g.
+  
+    export DB_URL="postgres://user:passwd@127.0.0.1:26257/db_name?sslmode=require&sslrootcert=./ca.crt
 
-$psql_templ = q[psql postgres://root@localhost:26257/defaultdb -tAc "WITH c AS (
+  ';
+}
+
+# "\timing on" in .psqlrc will break this
+$psqlrc = "$ENV{HOME}/.psqlrc";
+$s = -s $psqlrc;
+if ($s and $s > 0) {
+  rename $psqlrc, "$psqlrc.BAK";
+}
+
+$db_url = $ENV{DB_URL};
+
+$psql_templ = "psql '" . $db_url . "' -tAc " . q["WITH c AS (
       SELECT city, COUNT(*)
       FROM osm_names
       WHERE geohash5 = 'GEOHASH5'
@@ -12,6 +28,8 @@ $psql_templ = q[psql postgres://root@localhost:26257/defaultdb -tAc "WITH c AS (
       LIMIT 1
     )
     SELECT city FROM c;"];
+
+#print "psql_templ: $psql_templ\n";
 
 while (<>)
 {
